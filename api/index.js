39dff22fe9252b47fa126976942dbd8b37cc86f2
JCPrime01 <vanzokiller@gmail.com>
2026-06-13 20:25:32 -0300

    chore: shareCode LE5ZLHR3DNB -> 9UM4OO907W4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=LE5ZLHR3DNB&afp2=ti' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=9UM4OO907W4&afp2=ti' });
     res.end();
   }
